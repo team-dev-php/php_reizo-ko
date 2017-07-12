97ececa11f91dd8cef9b5035a1c8f5b23c1b3e74
SHOHEI-KOYAMA <s.koyama1011@gmail.com>
2017-07-12 20:59:00 +0900

    画面遷移完成

--- action_page/mypage.php
+++ action_page/mypage.php
@@ -27,19 +27,13 @@ $pdo = db_con();
     $stmt1->bindValue(':url', $url, PDO::PARAM_STR);
     $status = $stmt1->execute();//上記が全て実行された後の結果を$statusに格納
 
-    //User情報をDBで確認、null➜insert, あったらOK
-    $stmt3 = $pdo->prepare("SELECT * FROM user_table WHERE name = :user_name");
-    $stmt3->bindValue(':user_name', $user_name , PDO::PARAM_INT);
-    $status = $stmt3->execute();
-    if(isset($status)){
-
-    }else{
-        //nullの場合はuser情報を新規登録(Insert文)
-        $stmt2 = $pdo->prepare("INSERT INTO user_table(id, name, email, item_count, kanri_flg, life_flg)VALUES(NULL, :name, :email, NULL, NULL, NULL)");
-        $stmt2->bindValue(':name', $user_name, PDO::PARAM_STR);
-        $stmt2->bindValue(':email', $user_email, PDO::PARAM_STR);
-        $status = $stmt2->execute();
-    }
+    
+    //nullの場合はuser情報を新規登録(Insert文)
+    $stmt2 = $pdo->prepare("INSERT INTO user_table(name, email, item_count, kanri_flg, life_flg)VALUES(:name, :email, NULL, NULL, NULL)");
+    $stmt2->bindValue(':name', $user_name, PDO::PARAM_STR);
+    $stmt2->bindValue(':email', $user_email, PDO::PARAM_STR);
+    $status = $stmt2->execute();
+ 
 //2.user_nameからuser_idをDBから取得(select文)…いらないんじゃね?
     // $stmt3 = $pdo->prepare("SELECT 'name' FROM user_table WHERE 'name'= :user_name");
     // $stmt3->bindValue(':user_name', $user_name, PDO::PARAM_STR);
