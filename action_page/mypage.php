504c52f0bf55dc7510d767b2c96ca1473a19c2ff
SHOHEI-KOYAMA <s.koyama1011@gmail.com>
2017-07-13 13:28:04 +0900

    mypage.php若干修正その2

--- action_page/mypage.php
+++ action_page/mypage.php
@@ -160,21 +160,6 @@ if(isset($_SESSION["user_name"]) == ""){
                 </thead>
                 <tbody>
                 <?=$view?>
-                  <!-- <tr>
-                    <td>John</td>
-                    <td>Doe</td>
-                    <td>john@example.com</td>
-                  </tr>
-                  <tr>
-                    <td>Mary</td>
-                    <td>Moe</td>
-                    <td>mary@example.com</td>
-                  </tr>
-                  <tr>
-                    <td>July</td>
-                    <td>Dooley</td>
-                    <td>july@example.com</td>
-                  </tr> -->
                 </tbody>
               </table>
             </div>
