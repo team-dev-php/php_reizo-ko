08e86ab4ff7a12b9d6a56f79abe0249dcd3fcb25
SHOHEI-KOYAMA <s.koyama1011@gmail.com>
2017-07-13 18:19:14 +0900

    未完了箇所記載at item_conf.js

--- action_page/js/item_conf.js
+++ action_page/js/item_conf.js
@@ -1,6 +1,6 @@
 $(document).on(".item_comfirm","click",()=>{
-	let item_name = $("#mordal_item_name").txt();
-	let end_date  = $("#mordal_end_date").txt();
+	let item_name = $("#mordal_item_name").val();
+	let end_date  = $("#mordal_end_date").val();
 	console.log(item_name);
 	console.log(end_date);
 	$(".item_name").val(item_name);
