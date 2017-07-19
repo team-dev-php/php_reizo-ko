5e8b69ac583754df11940b96760b24451f230ef7
SHOHEI-KOYAMA <s.koyama1011@gmail.com>
2017-07-19 13:17:39 +0900

    ログイン修正

--- action_page/logincheck.php
+++ action_page/logincheck.php
@@ -4,6 +4,9 @@
 	$_SESSION['user_name'] = $_POST["user_name"];
 	$_SESSION['user_email'] = $_POST["user_email"];
 	$_SESSION['icon'] = $_POST['icon'];
+
+    $user_name = $_POST['user_name'];
+    $user_email = $_POST['user_email'];
 	//外部ファイル読み込み
     include("functions.php");
 
@@ -11,14 +14,14 @@
     $pdo = db_con();
 
     //1.ユーザー情報がすでにdbにあるかないか識別
-    $stmt1 = $pdo->prepare("SELECT TOP 1 * FROM user_table WHERE name = :user_name and email = :user_email");
-    $stmt1->bindValue(':user_name',$_POST['user_name'],PDO::PARAM_STR);
-    $stmt1->bindValue(':user_email',$_POST['user_email'],PDO::PARAM_STR);
-    $result = $stmt1->execute();
+    $stmt1 = $pdo->prepare("SELECT * FROM user_table WHERE name = :user_name and email = :user_email");
+    $stmt1->bindValue(':user_name',$user_name, PDO::PARAM_STR);
+    $stmt1->bindValue(':user_email',$user_email, PDO::PARAM_STR);
+    $result1 = $stmt1->execute();
     // while( $result = $stmt->fetch(PDO::FETCH_ASSOC)){
-
+    $val = $stmt1->fetch();
     // }
-    if(isset($result)){
+    if($val["id"] != ""){
     	//2.ある場合、値をdbから取得し、それをセッション変数に代入
     	$stmt2 = $pdo->prepare("SELECT `id`,`user_nickname` FROM user_table WHERE name = :user_name and email = :user_email");
 	    $stmt2->bindValue(':user_name',$_POST['user_name'],PDO::PARAM_STR);
@@ -31,9 +34,11 @@
 
     	}
     }else{
-    	//3.ない場合、dbに情報を登録し、そのid含む値をセッション変数に代入
-    	$stmt2 = $pdo->prepare("INSERT INTO user_table WHERE name = :user_name and email = :user_email");
+
+    	// 3.ない場合、dbに情報を登録し、そのid含む値をセッション変数に代入
+    	$stmt2 = $pdo->prepare("INSERT INTO user_table(`name`,`user_nickname`,`email`) VALUES (:user_name,:user_name,:user_email)");
 	    $stmt2->bindValue(':user_name',$_POST['user_name'],PDO::PARAM_STR);
+        $stmt2->bindValue(':user_name',$_POST['user_name'],PDO::PARAM_STR);
 	    $stmt2->bindValue(':user_email',$_POST['user_email'],PDO::PARAM_STR);
 	    $result2 = $stmt2->execute();
 
@@ -41,13 +46,13 @@
 	    $stmt3->bindValue(':user_name',$_POST['user_name'],PDO::PARAM_STR);
 	    $stmt3->bindValue(':user_email',$_POST['user_email'],PDO::PARAM_STR);
 
-	    $result = $stmt3->execute();
-	    while( $result = $stmt3->fetch(PDO::FETCH_ASSOC)){
-    		$_SESSION["user_id"] = $result["id"];
-    		$_SESSION["user_nickname"] = $result["user_nickname"];
-
+	    $result3 = $stmt3->execute();
+	    while( $result3 = $stmt3->fetch(PDO::FETCH_ASSOC)){
+    		$_SESSION["user_id"] = $result3["id"];
+    		$_SESSION["user_nickname"] = $result3["user_nickname"];
     	}
     }
+    // echo $val["id"];
      echo $_SESSION["user_id"];
     // $_SESSION["user_id"] = "2";
 	//５．index.phpへリダイレクト
--- action_page/mypage.php
+++ action_page/mypage.php
@@ -34,7 +34,18 @@
     <script src="js/reader.js"></script>
     <script src="js/recipe_generat.js"></script>
     <script src="js/file_upload.js"></script>
+    <script src="js/item_info_modal.js"></script>
     <!-- <script src="js/google_login.js"></script> -->
+
+    <script src="js/Chart.bundle.js"></script>
+    <script src="js/utils.js"></script>
+    <style>
+    canvas{
+        -moz-user-select: none;
+        -webkit-user-select: none;
+        -ms-user-select: none;
+    }
+    </style>
 </head>
 
 <body id="" class="index">
@@ -54,25 +65,24 @@
                 <!-- Add icon library -->
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
                 <div class="card">
-                    <h4>ユーザープロフィール</h4>
+                    <h4 style="padding: 5px 0;">Profile</h4>
                   <img src=<?=$icon?> alt="user_icon" class="user_icon" style="width:50%">
                     <h4><span class="update_name"><?=$user_nickname?></span></h4>
                     <h4>所属：<span class="update_belong_to"><?=$belong_to?></span></h4>
                     <h4>居住エリア:<span class="update_address"><?=$address?></span></h4>
                     <h4>大好物：<span class="update_favorite_dish"><?=$favorite_dish?></span></h4>
-                    <a href="#" class="sns_icon"><i class="fa fa-dribbble"></i></a>
-                    <a href="#" class="sns_icon"><i class="fa fa-twitter"></i></a>
-                    <a href="#" class="sns_icon"><i class="fa fa-linkedin"></i></a>
-                    <a href="#" class="sns_icon"><i class="fa fa-facebook"></i></a>
+                    <a href="#" class="sns_icon"><i class="fa fa-dribbble"></i></a> 
+                    <a href="#" class="sns_icon"><i class="fa fa-twitter"></i></a> 
+                    <a href="#" class="sns_icon"><i class="fa fa-linkedin"></i></a> 
+                    <a href="#" class="sns_icon"><i class="fa fa-facebook"></i></a> 
                     <p><button class="btn btn_block frigo_btn profile_edit">プロフィールを編集</button></p>
-
+                  
                 </div>
             </div>
             <!-- /プロフィールページ -->
-            <div class="back"><div id="box"></div></div>
             <!-- 残り物レシピ画像欄 -->
             <div class="container text-center">
-
+                <div class="back"><div id="box"></div></div>
             </div>
             <!-- /残り物レシピ画像欄 -->
             <div class="container text-center">
@@ -86,26 +96,142 @@
                         <th>オプション</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody id="item_list">
                         <?=$view?>
                     </tbody>
                 </table>
             </div>
             <div class="container text-center">
                 <h4>現在までの保存記録分析</h4>
-                <table class="table table-condensed">
-                    <thead>
-                        <!-- <tr>
-                        <th>Item Name</th>
-                        <th>保存開始日</th>
-                        <th>保存期限</th>
-                        <th>オプション</th>
-                        </tr> -->
-                    </thead>
-                    <tbody>
-                        <!-- <?=$itemlog?> -->
-                    </tbody>
-                </table>
+                <div style="width:75%;">
+        <canvas id="canvas"></canvas>
+    </div>
+    <br>
+    <br>
+    <button id="randomizeData">Randomize Data</button>
+    <button id="addDataset">Add Dataset</button>
+    <button id="removeDataset">Remove Dataset</button>
+    <button id="addData">Add Data</button>
+    <button id="removeData">Remove Data</button>
+    <script>
+        var MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        var config = {
+            type: 'line',
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [{
+                    label: "My First dataset",
+                    backgroundColor: window.chartColors.red,
+                    borderColor: window.chartColors.red,
+                    data: [
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor()
+                    ],
+                    fill: false,
+                }, {
+                    label: "My Second dataset",
+                    fill: false,
+                    backgroundColor: window.chartColors.blue,
+                    borderColor: window.chartColors.blue,
+                    data: [
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor()
+                    ],
+                }]
+            },
+            options: {
+                responsive: true,
+                title:{
+                    display:true,
+                    text:'Chart.js Line Chart'
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: false,
+                },
+                hover: {
+                    mode: 'nearest',
+                    intersect: true
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Month'
+                        }
+                    }],
+                    yAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Value'
+                        }
+                    }]
+                }
+            }
+        };
+        window.onload = function() {
+            var ctx = document.getElementById("canvas").getContext("2d");
+            window.myLine = new Chart(ctx, config);
+        };
+        document.getElementById('randomizeData').addEventListener('click', function() {
+            config.data.datasets.forEach(function(dataset) {
+                dataset.data = dataset.data.map(function() {
+                    return randomScalingFactor();
+                });
+            });
+            window.myLine.update();
+        });
+        var colorNames = Object.keys(window.chartColors);
+        document.getElementById('addDataset').addEventListener('click', function() {
+            var colorName = colorNames[config.data.datasets.length % colorNames.length];
+            var newColor = window.chartColors[colorName];
+            var newDataset = {
+                label: 'Dataset ' + config.data.datasets.length,
+                backgroundColor: newColor,
+                borderColor: newColor,
+                data: [],
+                fill: false
+            };
+            for (var index = 0; index < config.data.labels.length; ++index) {
+                newDataset.data.push(randomScalingFactor());
+            }
+            config.data.datasets.push(newDataset);
+            window.myLine.update();
+        });
+        document.getElementById('addData').addEventListener('click', function() {
+            if (config.data.datasets.length > 0) {
+                var month = MONTHS[config.data.labels.length % MONTHS.length];
+                config.data.labels.push(month);
+                config.data.datasets.forEach(function(dataset) {
+                    dataset.data.push(randomScalingFactor());
+                });
+                window.myLine.update();
+            }
+        });
+        document.getElementById('removeDataset').addEventListener('click', function() {
+            config.data.datasets.splice(0, 1);
+            window.myLine.update();
+        });
+        document.getElementById('removeData').addEventListener('click', function() {
+            config.data.labels.splice(-1, 1); // remove the label first
+            config.data.datasets.forEach(function(dataset, datasetIndex) {
+                dataset.data.pop();
+            });
+            window.myLine.update();
+        });
+    </script>
             </div>
 
         </div>
@@ -127,7 +253,7 @@
               <div class="modal-body row">
                 <div class="list-group">
                     <img src=<?=$icon?> alt="user_icon"  style="width:50%">
-                    <input type = "hidden" id="edit_img_src" value=<?=$icon?>>
+                    <input type = "hidden" id="edit_img_src" name="edit_img_src" value=<?=$icon?>>
                     <div class="d-flex w-100 justify-content-between">
                         <h5 class="mb-1">ユーザー名:</h5>
                     </div>
@@ -144,19 +270,19 @@
                         <h5 class="mb-1">居住エリア:</h5>
                     </div>
                     <p class="mb-1">
-                        <input type="text" name="address" value=<?=$address?> class="text-center" required>
+                        <input type="text" name="edit_address" value=<?=$address?> class="text-center" required>
                     </p>
                     <div class="d-flex w-100 justify-content-between">
                         <h5 class="mb-1">所属:</h5>
                     </div>
                     <p class="mb-1">
-                        <input type="text" name="belong_to" value=<?=$belong_to?> class="text-center" required>
+                        <input type="text" name="edit_belong_to" value=<?=$belong_to?> class="text-center" required>
                     </p>
                     <div class="d-flex w-100 justify-content-between">
                         <h5 class="mb-1">大好物:</h5>
                     </div>
                     <p class="mb-1">
-                        <input type="text" name="favorite_dish" value=<?=$favorite_dish?> class="text-center" required>
+                        <input type="text" name="edit_favorite_dish" value=<?=$favorite_dish?> class="text-center" required>
                     </p>
                 </div>
               </div>
@@ -169,9 +295,8 @@
       </div>
     </div>
     <!-- /modal window for profile_edit-->
-
+    
     <!-- modal window for item_info -->
-    <!-- modal window -->
     <div class="modal fade" id="item_modal">
       <div class="modal-dialog">
         <!-- <div class="modal-content"> -->
@@ -181,55 +306,45 @@
             <h4 class="modal-title text-center">アイテム情報</h4>
           </div>
           <form id="item_info_edit" action="item_info_edit.php" method="post" class="form">
-            <input type="hidden" name="item_id" value=<?=$id?> >
+            <input type="hidden" name="item_id" value="">
               <div class="modal-body row">
                 <div class="list-group">
-                    <img src=<?=$icon?> alt="user_icon"  style="width:50%">
-                    <input type = "hidden" id="edit_img_src" value=<?=$icon?>>
+                    <img src="" alt="item_src"  style="width:50%" class="edit_item_src">
+                    <input type = "hidden" id="edit_item_src" value="" class="edit_item_src">
                     <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1">ユーザー名:</h5>
+                        <h5 class="mb-1">アイテム名:</h5>
                     </div>
                     <p class="mb-1">
-                        <input type="text" name="edit_user_name" value=<?=$user_nickname?> class="text-center edit_user_name" required>
-                    </p>
-                    <!-- <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1">e-mail:</h5>
-                    </div> -->
-                    <!-- <p class="mb-1">
-                       <input type="text" name="edit_user_email" value=<?=$user_email?> class="text-center" required>
-                    </p> -->
-                    <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1">居住エリア:</h5>
-                    </div>
-                    <p class="mb-1">
-                        <input type="text" name="address" value=<?=$address?> class="text-center" required>
+                        <input type="text" name="edit_item_name" value="" class="text-center edit_item_name" required>
                     </p>
                     <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1">所属:</h5>
+                        <h5 class="mb-1">カテゴリー:</h5>
                     </div>
                     <p class="mb-1">
-                        <input type="text" name="belong_to" value=<?=$belong_to?> class="text-center" required>
+                        <input type="text" name="item_category" value="" class="text-center item_category" required>
                     </p>
                     <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1">大好物:</h5>
+                        <h5 class="mb-1">保存開始日：</h5>
                     </div>
                     <p class="mb-1">
-                        <input type="text" name="favorite_dish" value=<?=$favorite_dish?> class="text-center" required>
+                        <input type="text" name="indate" value="" class="text-center indate" required>
+                    </p>
+                    <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">保存終了日:</h5>
+                    </div>
+                    <p class="mb-1">
+                        <input type="text" name="end_date" value="" class="text-center end_date" required>
                     </p>
                 </div>
               </div>
-
-
-
               <div class="modal-footer">
-                <button type="button" id="item_info_modal" class="frigo_btn close btn btn-block " data-dismiss="modal" aria-label="Close"><span aria-hidden="true" class="save_text">保存</span></button>
+                <button type="button" id="save_iteminfo" class="frigo_btn close btn btn-block " data-dismiss="modal" aria-label="Close"><span aria-hidden="true" class="save_text">保存</span></button>
                 <!-- /DBへ登録する情報のinput -->
                </div>
            </form>
         </div>
       </div>
     </div>
-    <!-- /modal window -->
     <!-- /modal window for item_info -->
 
     <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
@@ -257,12 +372,10 @@
 
     <!-- プロフィール編集用js -->
     <script src="js/profile_edit.js"></script>
+
     <!--消費期限アラート-->
     <script src="js/alert.js"></script>
 
-    <script>
-
-    </script>
 
 </body>
 
